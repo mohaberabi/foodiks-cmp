e95bf6af197293cd067269448e7b2222b694362d
Mohab Erabi <mohaberabi98@icloud.com>
2024-12-06 20:08:45 +0200

    add shared backhandler

--- composeApp/src/commonMain/kotlin/com/mohaberabi/kmp/foodiks/features/layout/navigation/LayoutNavigation.kt
+++ composeApp/src/commonMain/kotlin/com/mohaberabi/kmp/foodiks/features/layout/navigation/LayoutNavigation.kt
@@ -1,4 +1,5 @@
 package com.mohaberabi.kmp.foodiks.features.layout.navigation
+
 import com.mohaberabi.kmp.foodiks.features.layout.screen.LayoutScreenRoot
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
@@ -6,9 +7,11 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.mohaberabi.kmp.foodiks.features.tables.presentation.navigation.TablesRoute
 
 import com.mohaberabi.kmp.foodiks.foodiks.navigation.isCurrentRoute
 import com.mohaberabi.kmp.foodiks.foodiks.navigation.navigateBottom
+import com.mohaberabi.kmp.foodiks.platform.AppBackHandler
 import kotlinx.serialization.Serializable
 
 
@@ -22,11 +25,11 @@ fun NavGraphBuilder.layoutScreen(
     val currentDestination: NavDestination? = layoutNavController
         .currentBackStackEntryAsState().value?.destination
 
-//    BackHandler(
-//        enabled = currentDestination.isCurrentRoute(TablesRoute::class),
-//    ) {
-//
-//    }
+    AppBackHandler(
+        enabled = currentDestination.isCurrentRoute(TablesRoute::class),
+    ) {
+
+    }
     LayoutScreenRoot(
         onNavigateBottom = { bottom ->
             layoutNavController.navigateBottom(bottom.route)
--- composeApp/src/commonMain/kotlin/com/mohaberabi/kmp/foodiks/features/onboarding/navigation/OnBoardingNavigation.kt
+++ composeApp/src/commonMain/kotlin/com/mohaberabi/kmp/foodiks/features/onboarding/navigation/OnBoardingNavigation.kt
@@ -3,6 +3,7 @@ package com.mohaberabi.kmp.foodiks.features.onboarding.navigation
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.mohaberabi.kmp.foodiks.features.onboarding.screen.OnBoardingScreen
+import com.mohaberabi.kmp.foodiks.platform.AppBackHandler
 import kotlinx.serialization.Serializable
 
 
@@ -13,9 +14,9 @@ data object OnBoardingRoute
 fun NavGraphBuilder.onBoardingScreen(
     onGetStarted: () -> Unit
 ) = composable<OnBoardingRoute> {
-//    BackHandler {
-//
-//    }
+    AppBackHandler {
+
+    }
     OnBoardingScreen(
         onGetStarted = onGetStarted
     )
